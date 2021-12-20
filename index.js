c28f91912ae539f7544e09bf97fa08eb42d8944c
princegobah <babynamek@gmail.com>
2021-12-20 03:04:32 -0500

    commit tree 49d7a3fde6ea707f15a1105761fc7fdce8953701

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+
+
+function findMatching(drivers,names){return drivers.filter(function(name){return name.toLowerCase()===names.toLowerCase()})}
+function fuzzyMatch(drivers,beg){return drivers.filter(name =>{return name[0]==beg[0]})}
+//console.log(findMatching(drivers,"Bobby"))
+//console.log(fuzzyMatch(drivers,"Sa"))
+
+ 
+function matchName(drivers,names){return drivers.filter(function (stuff){if(stuff.name===names){return stuff}})}
+console.log(matchName(drivers,'Bobby'))
